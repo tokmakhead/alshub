793d0824a48cf87e4f8c262a6364b7cabf324cd0
Ercan DELİCAN <ercan.delican@ttafsavunma.com.tr>
2026-03-25 11:29:31 +0300

    Add version tag for visibility debug

--- resources/views/admin/sources/index.blade.php
+++ resources/views/admin/sources/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Kaynak Yönetimi') }}
+            {{ __('Kaynak Yönetimi') }} <span class="text-red-500">[V-8821]</span>
         </h2>
     </x-slot>
 

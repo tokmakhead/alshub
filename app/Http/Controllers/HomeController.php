707b955dfaddff28c74640640d2f492d88a54a6c
Ercan DELİCAN <ercan.delican@ttafsavunma.com.tr>
2026-03-26 11:41:31 +0300

    Emergency Revert: Restored HomeController to fix site crash (403/500)

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,23 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Content;
-use App\Models\ResearchArticle;
 
 class HomeController extends Controller
 {
     public function index()
     {
-        // Get contents
-        $contents = Content::where('status', 'published')->latest()->take(6)->get();
-        // Get research articles
-        $research = ResearchArticle::where('status', 'published')->latest()->take(6)->get();
-        
-        // Merge and sort them by date
-        $latestContents = $contents->concat($research)->sortByDesc(function ($item) {
-            return $item->published_at ?? $item->created_at;
-        })->take(6);
-
+        $latestContents = \App\Models\Content::where('status', 'published')->latest()->take(6)->get();
         return view('frontend.home', compact('latestContents'));
     }
 
